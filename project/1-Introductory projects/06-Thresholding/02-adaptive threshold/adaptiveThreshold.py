896125a4faf5420a359cf9fb71735a0ad37e62cd
mahdisahraei <mahdi_sahrai@yahoo.com>
2022-11-08 22:08:01 +0330

    codes in the adaptivethreshold project was revised

--- project/1-Introductory projects/06-Thresholding/02-adaptive threshold/adaptiveThreshold.py
+++ project/1-Introductory projects/06-Thresholding/02-adaptive threshold/adaptiveThreshold.py
@@ -2,6 +2,7 @@ import cv2
 
 img = cv2.imread('sudoku.png', 0)
 
+#adaptiveThreshold is used to apply the thresholding
 th1 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 5)
 th2 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 5)
 
