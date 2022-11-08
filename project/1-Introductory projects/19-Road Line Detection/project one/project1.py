34715fd55a891fc438f00b678c334d8c6198ab9d
mahdisahraei <mahdi_sahrai@yahoo.com>
2022-11-08 11:00:43 +0330

    codes in the second Road Line Detection project was revised

--- project/1-Introductory projects/19-Road Line Detection/project one/project1.py
+++ project/1-Introductory projects/19-Road Line Detection/project one/project1.py
@@ -3,13 +3,13 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 img = cv2.imread('roadone.jpeg')
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 img = cv2.resize(img, (500, 500))
 
 
 def SetRegion(image, vertices):
     mask = np.zeros_like(image)  # Return an array of zeros with the same shape and type as a given array
-    match_mask_color = (255,) * 3
+    match_mask_color = (255,) * 3 #it replicates 255 for two times in an arr like it (255,255,255)
     cv2.fillPoly(mask, vertices,
                  match_mask_color)  # is used to draw filled polygons like rectangle, triangle, pentagon over an image
     masked_image = cv2.bitwise_and(image, mask)
@@ -25,7 +25,8 @@ def DrawingLines(img, lines):
 
         cv2.line(blank_image, (x1, y1), (x2, y2), (0, 255, 0), thickness=10)
 
-    img = cv2.addWeighted(img, 1, blank_image, 1, 0.5)
+    # gamma is scaler added to each sum
+    img = cv2.addWeighted(img, 1, blank_image, 1, 0.5) #addWeighted() helps in transition of the image to another.  in order to blend image 
     return img
 
 
@@ -38,6 +39,7 @@ color = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 canny = cv2.Canny(color, 100, 155)
 cropped = SetRegion(canny, np.array([areavertices], np.int32))
 
+#rho is measured in pixels and theta is measured in radians 
 houghline = cv2.HoughLinesP(cropped, rho=6, theta=np.pi / 180, threshold=160, lines=np.array([]),
                             minLineLength=100, maxLineGap=80)
 
