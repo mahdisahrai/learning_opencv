329b40ee8e514d56b820338057b7e30d0369111b
mahdisahraei <mahdi_sahrai@yahoo.com>
2022-11-08 10:25:22 +0330

    somethings in the first Road Line Detection project was revised

--- project/1-Introductory projects/19-Road Line Detection/step 1/detection.py
+++ project/1-Introductory projects/19-Road Line Detection/step 1/detection.py
@@ -3,15 +3,16 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 img = cv2.imread('roadone.jpeg')
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 img = cv2.resize(img, (500, 500))
 
 
 def SetRegion(image, vertices):
     mask = np.zeros_like(image) #Return an array of zeros with the same shape and type as a given array
-    match_mask_color = (255,) * 3
+    match_mask_color = (255,) * 3 #it replicates 255 for two times in an arr like it (255,255,255)
     cv2.fillPoly(mask, vertices, match_mask_color) #is used to draw filled polygons like rectangle, triangle, pentagon over an image
     masked_image = cv2.bitwise_and(image, mask)
+    print(masked_image)
     return masked_image
 
 
