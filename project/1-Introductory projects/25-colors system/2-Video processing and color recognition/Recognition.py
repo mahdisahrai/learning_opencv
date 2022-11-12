9758c593b2bb9c8bb2c50f44441770172545dd35
mahdisahraei <mahdi_sahrai@yahoo.com>
2022-11-12 22:37:46 +0330

    somethings in the second colors system project was revised

--- project/1-Introductory projects/25-colors system/2-Video processing and color recognition/Recognition.py
+++ project/1-Introductory projects/25-colors system/2-Video processing and color recognition/Recognition.py
@@ -8,8 +8,8 @@ while (True):
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
 
     # https://www.w3schools.com/colors/colors_rgb.asp  *  website RGB Calculator
-    lower_red = np.array([50, 50, 100])
-    upper_red = np.array([253, 191, 255])
+    lower_red = np.array([50, 50, 100]) #Bold color
+    upper_red = np.array([253, 191, 255]) #light color
 
     mask_detect = cv.inRange(hsv, lower_red, upper_red)  # detecting object
     bitwise_and = cv.bitwise_and(frame, frame, mask=mask_detect)
