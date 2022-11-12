e9c1d4066428e648ce28130f865e1a6f684c7e39
mahdisahraei <mahdi_sahrai@yahoo.com>
2022-11-12 21:00:23 +0330

    the first sample was done about removing object in image

---
+++ project/1-Introductory projects/30-removing object in image/sample one/Inpaint.py
@@ -0,0 +1,22 @@
+import cv2
+
+main_img = cv2.imread('back.jpg')
+clean_img = cv2.imread('backtwo.png')
+
+gray_mask = cv2.cvtColor(clean_img, cv2.COLOR_BGR2GRAY)
+_, mask = cv2.threshold(gray_mask, 240, 255, cv2.THRESH_BINARY)
+
+
+# about inpaint:
+#The function reconstructs the selected image area from the pixel near the area boundary. 
+#The function may be used to remove dust and scratches from a scanned photo, 
+# or to remove undesirable objects from still images or video
+inpaint = cv2.inpaint(main_img, mask, 7, flags=cv2.INPAINT_TELEA)
+
+
+cv2.imshow('main', main_img)
+cv2.imshow('cleaned', clean_img)
+cv2.imshow('final', inpaint)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
