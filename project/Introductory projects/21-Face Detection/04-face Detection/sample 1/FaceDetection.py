66e7330ae6b6efff7f89bb98af8cbe25091eb9e3
Mehdi Sahraeei <76227011+mehdisahraeei@users.noreply.github.com>
2023-09-23 20:34:58 +0330

    Update FaceDetection.py

--- project/Introductory projects/21-Face Detection/04-face Detection/sample 1/FaceDetection.py
+++ project/Introductory projects/21-Face Detection/04-face Detection/sample 1/FaceDetection.py
@@ -1,25 +1,35 @@
+# Import necessary libraries
 import cv2
 from cvzone.FaceDetectionModule import FaceDetector
 
+# Initialize the video capture and face detector
 capture = cv2.VideoCapture(0)
 detector = FaceDetector()
 
+# Main loop for capturing and processing frames
 while (capture.isOpened()):
+    # Read a frame from the camera
     ret, frame = capture.read()
+    
+    # Detect faces in the frame
     result, box = detector.findFaces(frame)
 
-
-    # for example: {'id': 0, 'bbox': (295, 267, 273, 273), 'score': [0.8099899888038635], 'center': (431, 403)}
+    # If faces are detected, process them
     if box:
+        # Print the bounding box information of the first detected face
         print(box[0])
         center = box[0]['center']
+        # Draw a green circle at the center of the detected face
         cv2.circle(frame, center, 5, (0, 250, 0), 1)
 
+    # Display the frame with detected faces
     cv2.imshow('frame', frame)
 
+    # Check for the 'Esc' key to exit the loop
     key = cv2.waitKey(10) & 0xFF
     if key == 27:
         break
 
+# Release the video capture and close all windows
 capture.release()
 cv2.destroyAllWindows()
